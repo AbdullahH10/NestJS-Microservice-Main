5db90ae8634296147adf55e88130b47e03078a3d
AbdullahH10 <AbdullahH10@outlook.com>
2022-09-17 15:45:00 +0600

    first init

---
+++ src/product/product.module.ts
@@ -0,0 +1,19 @@
+import { Product, ProductSchema } from './product.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Product.name,
+        schema: ProductSchema
+      }
+    ])
+  ],
+  controllers: [ProductController],
+  providers: [ProductService]
+})
+export class ProductModule {}
